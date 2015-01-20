3ce20db3c64f57c46af2e3a4b40a696aa9acd579
SpoooonBK <erodriguez927@gmail.com>
2015-01-19 22:58:29 -0500

    Worked on GameTimer

--- app/src/main/java/buttley/nyc/esteban/magicbeans/gamelogic/GameTimer.java
+++ app/src/main/java/buttley/nyc/esteban/magicbeans/gamelogic/GameTimer.java
@@ -4,4 +4,25 @@ package buttley.nyc.esteban.magicbeans.gamelogic;
  * Created by Spoooon on 1/19/2015.
  */
 public class GameTimer {
+
+    private long startTime;
+    private long endTime;
+    private float elapsedTime;
+
+    public void startTimer(){
+        startTime =System.currentTimeMillis();
+    }
+
+    public void endTimer(){
+        endTime=System.currentTimeMillis();
+    }
+
+    private void calculateElapsedTime(){
+        elapsedTime = (endTime - startTime)/1000;
+    }
+
+    public float getTime(){
+        return elapsedTime;
+    }
+
 }
